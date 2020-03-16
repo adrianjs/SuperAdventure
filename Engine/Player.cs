8ed7d6b925a0b835c65a7e7bff51861b900808c9
Adrian Junger Steffenakk <adrian.steffenakk@funn.no>
2020-03-16 11:27:13 +0100

    Add project files.

---
+++ Engine/Player.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Player : LivingCreature
+    {
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
+        public Location CurrentLocation { get; set; }
+
+        public Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints, int level) : base(currentHitPoints, maximumHitPoints)
+        {
+            Gold = gold;
+            ExperiencePoints = experiencePoints;
+            Level = level;
+
+            Inventory = new List<InventoryItem>();
+            Quests = new List<PlayerQuest>();
+        }
+
+        public bool HasRequiredItemToEnterThisLocation(Location location)
+        {
+            if(location.ItemRequiredToEnter == null)
+            {
+                // There is no required item for this location, so return "True"
+                return true;
+            }
+
+            // Check if player has required item in their inventory
+            foreach(InventoryItem ii in Inventory)
+            {
+                if(ii.Details.ID == location.ItemRequiredToEnter.ID)
+                {
+                    // We found the required item, so return "true"
+                    return true;
+                }
+            }
+
+            // We didn't find the required item, so return "false";
+            return false;
+        }
+
+        public bool HasThisQuest(Quest quest)
+        {
+            foreach(PlayerQuest playerQuest in Quests)
+            {
+                if(playerQuest.Details.ID == quest.ID)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool CompletedThisQuest(Quest quest)
+        {
+            foreach(PlayerQuest playerQuest in Quests)
+            {
+                if(playerQuest.Details.ID == quest.ID)
+                {
+                    return playerQuest.IsCompleted;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasAllQuestCompletionItems(Quest quest)
+        {
+            // Check if player has all the items required to complete quest
+            foreach(QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                bool foundItemInPlayersInventory = false;
+
+                // Check each item in player's inventory, to check that they have it and the correct amount
+                foreach(InventoryItem ii in Inventory)
+                {
+                    if(ii.Details.ID == qci.Details.ID)
+                    {
+                        // Player has item in their inventory
+                        foundItemInPlayersInventory = true;
+
+                        if(ii.Quantity < qci.Quantity)
+                        {
+                            // Player does not have enough of the item to complete quest
+                            return false;
+                        }
+                    }
+                }
+
+                // The player does not have any of the quest completion item in their inventory
+                if (!foundItemInPlayersInventory)
+                {
+                    return false;
+                }
+            }
+
+            // If we got here, that means the player has all the required items and enough of them to complete the quest
+            return true;
+        }
+
+        public void RemoveQuestCompletionItems(Quest quest)
+        {
+            foreach(QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                foreach(InventoryItem ii in Inventory)
+                {
+                    if(ii.Details.ID == qci.Details.ID)
+                    {
+                        // Subtract the quantity needed to complete quest from player's inventory
+                        ii.Quantity -= qci.Quantity;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void AddItemToInventory(Item itemToAdd)
+        {
+            foreach(InventoryItem ii in Inventory)
+            {
+                if(ii.Details.ID == itemToAdd.ID)
+                {
+                    // They already have the item, so increase quantity by one
+                    ii.Quantity++;
+
+                    return; // We added the item, and are done, so get out of function
+                }
+            }
+
+            // They didn't have the item, so add it and set quantity to 1
+            Inventory.Add(new InventoryItem(itemToAdd, 1));
+        }
+
+        public void MarkQuestCompleted(Quest quest)
+        {
+            // Find quest in player's quest list
+            foreach(PlayerQuest pq in Quests)
+            {
+                if(pq.Details.ID == quest.ID)
+                {
+                    // Mark it as completed
+                    pq.IsCompleted = true;
+
+                    return; // We found the quest and marked it complete, so get out of this function
+                }
+            }
+        }
+    }
+}
---
+++ SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,118 @@
+﻿using Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        private Player _player;
+        private Monster _currentMonster;
+
+        public SuperAdventure()
+        {
+            InitializeComponent();
+
+            _player = new Player(10, 10, 20, 0, 1);
+            MoveTo(World.LocationByID(World.LOCATION_ID_HOME));
+            _player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_RUSTY_SWORD), 1));
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+        private void BtnNorth_Click(object sender, EventArgs e)
+        {
+            MoveTo(_player.CurrentLocation.LocationToNorth);
+        }
+
+        private void BtnEast_Click(object sender, EventArgs e)
+        {
+            MoveTo(_player.CurrentLocation.LocationToEast);
+        }
+
+        private void BtnSouth_Click(object sender, EventArgs e)
+        {
+            MoveTo(_player.CurrentLocation.LocationToSouth);
+        }
+
+        private void BtnWest_Click(object sender, EventArgs e)
+        {
+            MoveTo(_player.CurrentLocation.LocationToWest);
+        }
+
+        private void MoveTo(Location newLocation)
+        {
+            // Does the location have any required items
+            if (!_player.HasRequiredItemToEnterThisLocation(newLocation))
+            {
+                rtbMessages.Text += "You must have a " + newLocation.ItemRequiredToEnter.Name + " to enter this location." + Environment.NewLine;
+                return;
+            }
+
+            // Update the player's current location
+            _player.CurrentLocation = newLocation;
+
+            // Show/hide available movement buttons
+            btnNorth.Visible = (newLocation.LocationToNorth != null);
+            btnEast.Visible = (newLocation.LocationToEast != null);
+            btnWest.Visible = (newLocation.LocationToWest != null);
+            btnSouth.Visible = (newLocation.LocationToSouth != null);
+
+            // Display current location name and description
+            rtbLocation.Text = newLocation.Name + Environment.NewLine;
+            rtbLocation.Text += newLocation.Description + Environment.NewLine;
+
+            // Completely heal the player
+            _player.CurrentHitPoints = _player.MaximumHitPoints;
+
+            // Update Hit Points in UI
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+
+            // Does the new location have a quest?
+            if (newLocation.QuestAvailableHere != null)
+            {
+                // Check if player already has quest, and if completed
+                bool playerAlreadyHasQuest = _player.HasThisQuest(newLocation.QuestAvailableHere);
+                bool playerAlreadyCompletedQuest = _player.CompletedThisQuest(newLocation.QuestAvailableHere);
+
+                // See if the player already has the quest
+                if (playerAlreadyHasQuest)
+                {
+                    //If the player has not already completed quest
+                    if (!playerAlreadyCompletedQuest)
+                    {
+                        // Check if player has all required items to complete quest
+                        bool playerHasAllItemsToCompleteQuest = _player.HasAllQuestCompletionItems(newLocation.QuestAvailableHere);
+
+                        // Player has all items required to complete quest
+                        if (playerHasAllItemsToCompleteQuest)
+                        {
+                            // Display message
+                            rtbMessages.Text += Environment.NewLine;
+                            rtbMessages.Text += "You complete the "
+                        }
+                    }
+                }
+            }
+        }
+        private void BtnUseWeapon_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnUsePotion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
